f7808a95a510bb6eb7940e7926b7272e80491b51
shereen_28 <gmshereen98@gmail.com>
2022-02-09 12:34:53 +0200

    dashboard and left sidebar cimmit

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,8 +1,10 @@
+import { LeftSidebarComponent } from './../components/dashboard/left-sidebar/left-sidebar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateUserComponent } from '../components/user/create-user/create-user.component';
 
 const routes: Routes = [
+    {path:'dashboard',component:LeftSidebarComponent},
     // {
     //     path: 'user', 
        
