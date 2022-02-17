1986fb1c84f4d5c7948eda0bce1243ca1dbeeff4
marianashawky <marianashawky@gmail.com>
2022-02-17 18:37:11 +0200

    search componant

--- src/app/components/navigation-header/navigation-header.component.ts
+++ src/app/components/navigation-header/navigation-header.component.ts
@@ -1,12 +1,13 @@
 import { Page } from './../../models/page';
 import { Post } from './../../models/post';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { ApiService } from 'src/app/services/api.service';
 import { UserService } from 'src/app/services/user.service';
 import { environment } from 'src/environments/environment';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-navigation-header',
   templateUrl: './navigation-header.component.html',
@@ -15,9 +16,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class NavigationHeaderComponent implements OnInit {
 
   isLogged = false;
-  user: User = new User;
-  post:Post= new Post;
-  page:Page=new Page;
+  userName: User[]= [];
+  user:User =new User;
+  post:Post[]= [];
+  page:Page[]=[];
+  /// @Output() userName: EventEmitter<any> = new EventEmitter<any>(); 
   formsearch = new FormGroup({});
   storageURL = environment.storage_URL
   constructor(private _userService: UserService, private _router: Router, private _apiService: ApiService, private _formBuilder: FormBuilder) { }
@@ -41,23 +44,22 @@ export class NavigationHeaderComponent implements OnInit {
   }
 
   search(): void {
-
     //alert(JSON.stringify(this.formRegister.value));
     //Call API to create user    
     this._apiService.getName(`search`,this.formsearch.value.search)
     .subscribe(
       (response: any) => {
         alert(JSON.stringify(response));
-        this.user=response;
-        this.post=response;
-        this.page=response;
+       // this.userName.emit(this.userName); 
+        this.userName=response.resultUser;
+        this.post=response.resultPost;
+        this.page=response.resultPage;
         this._router.navigateByUrl (`search`);
       },
       (error: any) => {
         alert(error)
       }
       );
-
   }
 
   isValidControl(name: string): boolean {
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Page} from 'src/app/models/page'
+import { Post } from 'src/app/models/post';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { ApiService } from 'src/app/services/api.service';
+import { UserService } from 'src/app/services/user.service';
+import { environment } from 'src/environments/environment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +15,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  userName: User[]= [];
+  user:User =new User;
+  post:Post[]= [];
+  page:Page[]=[];
+  /// @Output() userName: EventEmitter<any> = new EventEmitter<any>(); 
+  formsearch = new FormGroup({});
+  storageURL = environment.storage_URL
+
+  constructor(private _userService: UserService, private _router: Router, private _apiService: ApiService, private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formsearch = this._formBuilder.group({
+
+      search: ['', [Validators.required]],
+
+
+    });
+    this._apiService.getOne("users",parseInt(localStorage.getItem('id')))
+    .subscribe((response:any) => this.user=response)
+
+
+
+  }
+  search(): void {
+    //alert(JSON.stringify(this.formRegister.value));
+    //Call API to create user    
+    this._apiService.getName(`search`,this.formsearch.value.search)
+    .subscribe(
+      (response: any) => {
+        alert(JSON.stringify(response));
+       // this.userName.emit(this.userName); 
+        this.userName=response.resultUser;
+        this.post=response.resultPost;
+        this.page=response.resultPage;
+        this._router.navigateByUrl (`search`);
+      },
+      (error: any) => {
+        alert(error)
+      }
+      );
+  }
+  unfriend(friendId){
+    this._apiService.get('friends')
+    .subscribe((friends:any)=>
+    {
+      let friendship_id = friends.filter((friendship:any)=> 
+      {return (friendship.user_id==parseInt(localStorage.getItem('id')) && friendship.friend_id==friendId)
+              || (friendship.friend_id==parseInt(localStorage.getItem('id')) && friendship.user_id==friendId)
+    })[0].id
+    // delete friendship
+    // this._apiService.delete('friends',friendship_id).subscribe((response:any)=>{
+    //   this.ngOnInit()
+    // },(error:any)=>{})
+    })
   }
 
 }
