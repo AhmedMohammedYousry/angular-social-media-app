bea0b400fafb20662608c85a0059412b7b97e3b3
shereen_28 <gmshereen98@gmail.com>
2022-03-04 20:25:46 +0200

    admin panle auth small edit

--- src/app/admin-auth.service.ts
+++ src/app/admin-auth.service.ts
@@ -1,24 +1,15 @@
 import { ApiService } from './services/api.service';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Injectable, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { CanActivate } from '@angular/router';
+import { Injectable } from '@angular/core';
 import { User } from './models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAuthService implements CanActivate{
-  
-  private isAdmin =new BehaviorSubject<boolean>(false);
-  admin:boolean=false;
-  current = this.isAdmin.asObservable();
- 
-  changeValue(newValue:boolean){
-    this.isAdmin.next(newValue);
-  }
-  constructor(private api: ApiService) { }
 
-  
+  admin:boolean=false;
+  constructor(private api: ApiService) { }
   canActivate(){
    
     this.api.getOne('users',parseInt(localStorage.getItem('id'))).subscribe(
